98a75c347b772f5f9d2a9446f2ca6feaaa54e497
Hermiltonian <hiroshi.tech.expert.0127@gmail.com>
2018-10-25 08:22:45 +0900

    ユーザ詳細ページを表示できるようにした

--- app.js
+++ app.js
@@ -1,17 +1,35 @@
+const userData = [
+  {
+    id: 1,
+    name: "Milk",
+    description: "I have a pattern like a cow.",
+  },
+  {
+    id: 2,
+    name: "Kaka",
+    description: "I like eating adn sleeping.",
+  },
+];
+
 const getUsers = (callback) => {
   setTimeout(() => {
-    callback(null, [
-      {
-        id: 1,
-        name: "Milk",
-        description: "I have a pattern like a cow.",
-      },
-      {
-        id: 2,
-        name: "Kaka",
-        description: "I like eating adn sleeping.",
-      },
-    ])
+    callback(null, userData)
+  }, 1000);
+}
+
+const getUser = (userId, callback) => {
+  setTimeout(() => {
+    const filteredUsers = userData.filter((user) => {
+      return user.id === parseInt(userId, 10);
+    });
+
+    if (filteredUsers.length > 0) {
+      callback(null, filteredUsers[0]);
+    } else {
+      message = "Error: ";
+      message += `User whose id is ${userId} does not exitst.`;
+      callback(message, null);
+    }
   }, 1000);
 }
 
@@ -41,6 +59,38 @@ var UserList = {
           this.error = error.toString();
         } else {
           this.users = users;
+        }
+      }).bind(this));
+    },
+  },
+}
+
+const UserDetail = {
+  template: "#user-detail",
+  data: function () {
+    return {
+      loading: false,
+      error: null,
+      user: null,
+    }
+  },
+  created: function () {
+    this.fetchData();
+  },
+  watch: {
+    "$route": "fetchData",
+  },
+  methods: {
+    fetchData: function () {
+      this.loading = true;
+
+      getUser(this.$route.params.userId, ((error, user) => {
+        this.loading = false;
+
+        if (error) {
+          this.error = error.toString();
+        } else {
+          this.user = user;
         }
       }).bind(this));
     },
