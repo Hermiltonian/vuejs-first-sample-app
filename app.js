b59e4cc95e5b8476f1fbc489ade4b19570b0f121
Hermiltonian <hiroshi.tech.expert.0127@gmail.com>
2018-11-05 20:58:25 +0900

    ログインコンポーネントを追加

--- app.js
+++ app.js
@@ -17,6 +17,28 @@ const Auth = {
   },
 }
 
+const Login = {
+  template: "#login",
+  data: function () {
+    return {
+      email: "vue@example.com",
+      pass: "",
+      error: false,
+    };
+  },
+  methods: {
+    login: function() {
+      Auth.login(this.email, this.pass, ((loggedIn) => {
+        if (!loggedIn) {
+          this.error = true;
+        } else {
+          this.$router.replace(this.$route.query.redirect ||  "/");
+        }
+      }).bind(this));
+    },
+  },
+}
+
 const userData = [
   {
     id: 1,
